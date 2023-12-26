f2ff62121fb676231734077dec58bec13dbd4e01
Lakshay <lakshaykhokhar2003@gmail.com>
2023-12-26 15:11:19 +0530

    Revert "cleanup!"
    This reverts commit 9f56faf4a486dc50b6d765557aa0167a5fa98ec2.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import Posts from "./pages/myposts/Posts";
 import Profile from "./pages/profile/Profile";
 import Likes from "./pages/likes/Likes";
 import NewPost from "./pages/createPost/NewPost";
-import {Flip, ToastContainer} from "react-toastify";
 
 const routes = [
     {
@@ -68,7 +67,6 @@ function App() {
     return (
         <Provider store={store}>
             <RouterProvider router={router}/>
-            <ToastContainer transition={Flip} position="top-right" limit={2} theme="light"/>
         </Provider>
     );
 }
